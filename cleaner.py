aa7dffe0eaae1e7e91086de05badcd57fb36e416
Matvey Kurzukov <mkurg@ya.ru>
2015-12-14 15:26:47 +0300

    new words2

--- cleaner.py
+++ cleaner.py
@@ -3,16 +3,22 @@ import re
 
 a = []
 with open('test2-cleaned.txt', 'r') as inp:
-	for line in inp:
-		a.append(line)
+    for line in inp:
+        a.append(line)
 '''
 for line in a:
-	line = re.sub('\t\d+\t/.*$', '', line)
+    line = re.sub('\t\d+\t/.*$', '', line)
 '''
-with open('test2-words-2.txt', 'w') as out:
-	for line in a:
-		z = re.findall('\w+\.\w+', line)
-		if len(z) > 0:
-			for l in z:
-				out.write(l)
-				out.write('\n')
+freqDict = {}
+
+for line in a:
+    z = re.findall('\w+і\w+', line)
+    if len(z) > 0:
+        for l in z:
+                if l in freqDict:
+                    freqDict[l] += 1
+                else:
+                    freqDict[l] = 1
+with open('poi.txt', 'w') as out:
+    for word in sorted(freqDict, key = lambda x: freqDict[x], reverse=True):
+        out.write(word + ':' + str(freqDict[word]) + '\n')
